d8d57da2903213a30604e4629571f75c1ef0b8b4
Tyler Quinn <adminaccountrun@Tylers-MacBook-Pro-3.local>
2022-05-19 21:32:53 -0500

    Initial Commit

---
+++ index.js
@@ -0,0 +1,46 @@
+// 37:45 
+
+const express = require('express')
+app = express()
+
+var url = require('url');
+var dt = require('./date-time');
+
+const port = process.env.PORT || 3000
+const majorVersion = 1
+const minorVersion = 2
+
+// Use Express to publish static HTML, CSS, and JavaScript files that run in the browser. 
+app.use(express.static(__dirname + '/static'))
+
+//HTML quiz app on the server
+app.get('/html', (request, response) => {
+	app.use(express.static(__dirname + '/html'))
+    response.sendFile(path.join(__dirname + '/teamsknoweachother/index.html'))
+})
+
+//CSS quiz app on the server
+app.get('/css', (request, response) => {
+	app.use(express.static(__dirname + '/css'))
+    response.sendFile(path.join(__dirname + '/css/index.html'))
+})
+
+// Custom 404 page.
+app.use((request, response) => {
+  response.type('text/plain')
+  response.status(404)
+  response.send('404 - Not Found')
+})
+
+// Custom 500 page.
+app.use((err, request, response, next) => {
+  console.error(err.message)
+  response.type('text/plain')
+  response.status(500)
+  response.send('500 - Server Error')
+})
+
+app.listen(port, () => console.log(
+  `Express started at \"http://localhost:${port}\"\n` +
+  `press Ctrl-C to terminate.`)
+)
---
+++ static/html/question.js
@@ -0,0 +1,31 @@
+
+
+// Array of Objects 
+const quiz = [
+    {
+        q:'Which month comes right before June? ',
+        options:['May','September','July','August'],
+        answer:0 
+    },
+    {
+        q:'What color is a banana?',
+        options:['Red','Yellow','Blue','White'],
+        answer:1
+    },
+    {
+        q:'3 + 4 = 7 ?',
+        options:['True','False'],
+        answer:0
+    },
+    {
+        q:'What time of the day is breakfast?',
+        options:['Afternoon','Evening','Morning'],
+        answer:2
+    },
+    {
+        q:'What is 22 + 6 ?',
+        options:['99','28','21'],
+        answer:1
+    }
+    
+]
