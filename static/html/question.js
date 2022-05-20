a814d56c74ad7c10cc1c040ed2c8d174e31667d2
Tyler Quinn <adminaccountrun@Tylers-MacBook-Pro-3.local>
2022-05-19 23:02:34 -0500

    Added a handful of starter html quiz questions

--- static/html/question.js
+++ static/html/question.js
@@ -3,29 +3,85 @@
 // Array of Objects 
 const quiz = [
     {
-        q:'Which month comes right before June? ',
-        options:['May','September','July','August'],
+        q:'What type of tag defines headings in html? ',
+        options:['&lth1&gt','&ltp&gt','&ltli&gt'],
         answer:0 
     },
     {
-        q:'What color is a banana?',
-        options:['Red','Yellow','Blue','White'],
-        answer:1
-    },
-    {
-        q:'3 + 4 = 7 ?',
-        options:['True','False'],
+        q:'Is &ltbr&gt a self-closing tag?',
+        options:['Yes','No'],
         answer:0
     },
     {
-        q:'What time of the day is breakfast?',
-        options:['Afternoon','Evening','Morning'],
-        answer:2
+        q:'Is &lth1&gt a self-closing tag?',
+        options:['No','Yes'],
+        answer:0
     },
     {
-        q:'What is 22 + 6 ?',
-        options:['99','28','21'],
-        answer:1
-    }
-    
+        q:'What does a "&ltbr&gt" tag do?',
+        options:['Produces a line break in text','Adds inline text','Enables flex content','Adds a horizontal line to break between paragraph-level elements'],
+        answer:0
+    },
+    {
+        q:'What does the &lthr&gt tag do?',
+        options:['Adds a horizontal line to break between paragraph-level elements','Produces a line break in text','Adds inline text','Enables flex content'],
+        answer:0
+    },
+    {
+        q:'What is an html attribute?',
+        options:['Text which gives information to the browser about modifications to the element','Text which defines page alignment for the element'],
+        answer:0
+    },
+    {
+        q:'What does the html attribute "align" specify?',
+        options:['Sets the alignment of the rule on the page','Sets the height in pixels of the rule'],
+        answer:0
+    },
+    {
+        q:'What is the default value for the "align" attribute of a html element when no specification is given?',
+        options:['Left','Center','Right'],
+        answer:0
+    },
+    {
+        q:'What does the html attribute "size" specify?',
+        options:['Sets the height, in pixels, of the rule','Sets the width, in pixels, of the rule','Sets the height, in percentage, of the rule','Sets the width, in percentage, of the rule'],
+        answer:0
+    },
+    {
+        q:'What command gets boilerplate html code in Visual Studio Code? (assuming a html document)',
+        options:['! enter ','shift enter'],
+        answer:0
+    },
+    {
+        q:'How do you build an unordered list of items in html?',
+        options:['&ltul&gt tags enclosing &ltli&gt tag items','&ltli&gt tags enclosing &ltul&gt tag items'],
+        answer:0
+    },
+    {
+        q:'How do you build an ordered list of items in html?',
+        options:['&ltol&gt tags enclosing &ltli&gt tag items','&ltli&gt tags enclosing &ltol&gt tag items','&ltul&gt tags enclosing &ltli&gt tag items','&ltli&gt tags enclosing &ltul&gt tag items'],
+        answer:0
+    },
+    {
+        q:'What attribute is given to an &ltimg&gt tag to specify the image being pulled?',
+        options:['&ltimg src="xyz.png"&gt','&ltimg url="xyz.png"&gt','&ltimg href="xyz.png"&gt'],
+        answer:0
+    },
+    {
+        q:'What attribute is given to an &ltimg&gt tag to display text for images which cannot be reached?',
+        options:['&ltimg alt="text"&gt','&ltimg txt="text"&gt'],
+        answer:0
+    },
+    {
+        q:'What tag is added to implement a hyperlink?',
+        options:['&lta&gt','&ltli&gt'],
+        answer:0
+    },
+    {
+        q:'What attribute is given to an &lta&gt hyperlink anchor tag to specify the link?',
+        options:['&ltimg href="https://google.com"&gt','&ltimg url="https://google.com"&gt','&ltimg src="https://google.com"&gt'],
+        answer:0
+    },
+
+
 ]
