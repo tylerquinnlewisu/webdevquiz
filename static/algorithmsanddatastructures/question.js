9e6e17d2b42ecd455d39ca7114cc695d0388137f
Tyler Quinn <adminaccountrun@Tylers-MacBook-Pro-3.local>
2022-08-14 16:03:10 -0500

    Updated home page and added navbar

--- static/algorithmsanddatastructures/question.js
+++ static/algorithmsanddatastructures/question.js
@@ -3,9 +3,56 @@
 // Array of Objects 
 const quiz = [
     {
-        q:'What is Bootstrap? ',
-        options:['A front-end library for JavaScript','A programming language'],
+        q:'What is an array? ',
+        options:['An array is a collection of items of a specific type for most languages.','An array is a collection of integers with named indexes.'],
         answer:0 
     },
+    {
+        q:'How would you use a for loop to cycle through an array in JavaScript? ',
+        options:[" for (let i = 0; i < array.length; i++) { }",' for (let i = 0; i < array.length - 1; i++) { } '],
+        answer:0 
+    },
+    {
+        q:'How would you add new elements to an array in JavaScript? ',
+        options:['array.push(element)','array.append(element)'],
+        answer:0 
+    },
+    {
+        q:'True or False: JavaScript supports arrays with named indexes ',
+        options:['False - JavaScript arrays always use numbered indexes','True - JavaScript uses arrays with named indexes'],
+        answer:0 
+    },
+    {
+        q:'What is an array with named indexes called?',
+        options:['An associative array','An object'],
+        answer:0 
+    },
+    {
+        q:'What is the difference between storage and RAM?',
+        options:['Memory (RAM) information will disappear when the power is turned off, but storage is persistent.','Storage information will disappear when the power is turned off, but Memory (RAM) is persistent.'],
+        answer:0 
+    },
+    {
+        q:'What is the primary downside to storage since it is persistent (as opposed to memory)?',
+        options:['Reading data from/to storage is generally slow.','Reading data from/to storage introduces the potential for security breaches.'],
+        answer:0 
+    },
+    {
+        q:'A byte is a small unit of data representing how many bits?',
+        options:['8','6','3','12','24'],
+        answer:0 
+    },
+    {
+        q:'A bit is represented as what?',
+        options:['1 or 0',''],
+        answer:0 
+    },
+    {
+        q:'What is the simplest type of data structure?',
+        options:['Linear array (one-dimensional array)','Linked list','Tree'],
+        answer:0 
+    },
+
+
 
 ]
